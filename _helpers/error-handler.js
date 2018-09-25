dc872c80532bdd8d54703a6b2ec08782a5c428ef
janton42 <jantonstock@gmail.com>
2018-09-24 21:58:46 -0700

    added db connection and error handlers

--- _helpers/error-handler.js
+++ _helpers/error-handler.js
@@ -1,1 +1,21 @@
-//error-handler 
+module.exports = errorHandler;
+
+function errorHandler(err, req, res, next) {
+	if (typeof (err) === 'string') {
+		// custom application error
+		return res.status(400).json({ message: err });
+	}
+
+	if (err.name === 'ValidationError') {
+		// mongoose validation error
+		return res.status(400).json({ message: err});
+	}
+
+	if (err.name === 'UnauthorizedError') {
+		// jwt authentication error
+		return res.status(401).json({ message: 'Invalid token' });
+	} 
+
+	// default to 500 server error
+	return res.status(500).json({ message: err.message });
+}
